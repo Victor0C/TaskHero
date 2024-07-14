42eb5e5b1228f30ff8821d50a44b01bbd262949a
Victor Hugo <hugovco511@gmail.com>
2024-07-14 18:44:04 -0300

    feat: delete modal component layout

---
+++ frontend/src/components/modals/ModalDeleteTask.vue
@@ -0,0 +1,49 @@
+<template>
+  <button
+    type="button"
+    class="btn btn-danger myButton"
+    data-bs-toggle="modal"
+    :data-bs-target="`#${task.id}deleteModal`"
+  >
+    Deletar
+  </button>
+  <div
+    class="modal fade"
+    :id="`${task.id}deleteModal`"
+    tabindex="-1"
+    :aria-labelledby="`#${task.id}deleteModalLabel`"
+    aria-hidden="true"
+  >
+    <div class="modal-dialog">
+      <div class="modal-content">
+        <div class="modal-header">
+          <h1 class="modal-title fs-5" :id="`${task.id}deleteModalLabel`">
+            Deletar a tarefa de {{ task.title }}
+          </h1>
+          <button
+            type="button"
+            class="btn-close"
+            data-bs-dismiss="modal"
+            aria-label="Close"
+          ></button>
+        </div>
+        <div class="modal-body">
+          <h1>Corpo do modal de deleção de tarefa</h1>
+        </div>
+        <div class="modal-footer">
+          <button type="button" class="btn btn-danger" data-bs-dismiss="modal">Deletar</button>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'ModalDeleteTask',
+  emits: ['taskDeleted'],
+  props: {
+    task: null
+  }
+}
+</script>
